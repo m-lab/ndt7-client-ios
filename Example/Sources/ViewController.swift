603c4f584c332b25e899eb260429f3664ea679f6
miguelangelnet <48926351+miguelangelnet@users.noreply.github.com>
2019-03-29 15:57:55 -0700

    Framework and example setup (#2)
    * FrameworkAndExampleSetup - workspace and projects created
    * FrameworkAndExampleSetup - targets and build settings setup for ndt7 project
    * FrameworkAndExampleSetup - finishing workspace setup
    * FrameworkAndExampleSetup - houndci review

---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ndt7 iOS Example
+//
+//  Created by Miguel on 3/29/19.
+//  Copyright © 2019 M-Lab. All rights reserved.
+//
+
+import UIKit
+import ndt7
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
