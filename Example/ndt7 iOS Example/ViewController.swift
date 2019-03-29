60013a247d3ba26f9f14c9406c87b931eeaf3446
Miguel Nieto <miguelangelnet@gmail.com>
2019-03-29 12:39:21 -0700

    FrameworkAndExampleSetup - workspace and projects created

---
+++ Example/ndt7 iOS Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ndt7 iOS Example
+//
+//  Created by Miguel on 3/29/19.
+//  Copyright © 2019 M-Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sources/ndt7.h
@@ -0,0 +1,19 @@
+//
+//  ndt7.h
+//  ndt7
+//
+//  Created by Miguel on 3/29/19.
+//  Copyright © 2019 M-Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ndt7.
+FOUNDATION_EXPORT double ndt7VersionNumber;
+
+//! Project version string for ndt7.
+FOUNDATION_EXPORT const unsigned char ndt7VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ndt7/PublicHeader.h>
+
+
