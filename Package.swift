bb45ded651195cab35d03d36a661c3d980844d67
Miguel Nieto <miguelangelnet@gmail.com>
2019-04-05 11:38:49 -0700

    SwiftPackageManagerSetup - Package.swift added

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.0
+//
+//  NDT7.swift
+//  NDT7 iOS
+//
+//  Created by Miguel on 4/5/19.
+//  Copyright © 2019 M-Lab. All rights reserved.
+//
+
+
+import PackageDescription
+
+let package = Package(
+    name: "NDT7",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "NDT7",
+            targets: ["NDT7"])
+    ],
+    targets: [
+        .target(
+            name: "NDT7",
+            path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
+)
