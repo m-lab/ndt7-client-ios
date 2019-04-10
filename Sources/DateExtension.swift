9a3de0858250c7c2dcd94cf6364fb61fd1c1ee0b
Miguel Nieto <miguelangelnet@gmail.com>
2019-04-10 15:57:47 -0700

    NDT7Logger - NDT7Logger

---
+++ Sources/DateExtension.swift
@@ -0,0 +1,32 @@
+//
+//  DateExtension.swift
+//  NDT7
+//
+//  Created by NietoGuillen, Miguel on 4/8/19.
+//  Copyright © 2019 M-Lab. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Date Extensions
+
+/// Date Extension
+extension Date {
+    
+    /// Date and time format for log message.
+    static let logStringDateFormat = "yyyy-MM-dd HH:mm:ss.SSSSSSZ"
+    
+    /// DateFormatter for log message.
+    static let logDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = logStringDateFormat
+        return dateFormatter
+    }()
+    
+    /// Convert date to string using specific date format.
+    /// - parameter dateFormatter: date format as DateFormatter. Default: DateFormatter for log message.
+    /// - returns: string with specific format.
+    func toString(dateFormatter: DateFormatter = Date.logDateFormatter) -> String {
+        return dateFormatter.string(from: self)
+    }
+}
