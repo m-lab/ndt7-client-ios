0c25279c1d4d36fc7de5f4093b1bb5fb99eee170
Olga Galchenko <olga.galchenko@airbnb.com>
2022-08-03 12:42:08 -0700

    eek broke it on xcode 13

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -11,7 +11,11 @@ import Foundation
 /// Protocol for URLSession dataTask
 public protocol URLSessionNDT7 {
     associatedtype DataTaskType: URLSessionTaskNDT7
-    func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> DataTaskType
+  @available (iOS 16, *)
+  func dataTask(with request: URLRequest, completion: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> DataTaskType
+
+  @available(iOS, obsoleted: 16)
+  func dataTask(with request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> DataTaskType
 }
 
 /// Protocol for URLSessionTask
@@ -22,6 +26,15 @@ public protocol URLSessionTaskNDT7 {
 }
 
 extension URLSession: URLSessionNDT7 {
+  @available (iOS 16, *)
+  public func dataTask(with request: URLRequest, completion: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+   dataTask(with: request, completionHandler: completion)
+  }
+
+  @available(iOS, obsoleted: 16)
+  public func dataTask(with request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    dataTask(with: request, completionHandler: completion)
+  }
 }
 
 extension URLSessionTask: URLSessionTaskNDT7 {
